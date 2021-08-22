985f8301d81dd880d903258e05d59ce842807c96
Colin Hoolwerf <choolwerf@gmail.com>
2021-08-22 11:04:32 -0500

    ready to start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='container'>
+      <h1>Colin Hoolwerf</h1>
     </div>
   );
 }
